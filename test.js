e60bd378acab0f586d5090a4370e7d617e74ddeb
Ben Drucker <bvdrucker@gmail.com>
2018-11-13 22:35:59 -0800

    standard --fix

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var inject = require('shot').inject
 var assertPath = require('./')
 
 test('valid path', function (t) {
-  inject(dispatch, {url: '/boop'}, t.fail)
+  inject(dispatch, { url: '/boop' }, t.fail)
 
   function dispatch (req, res) {
     assertPath(req, res, t.end)
@@ -15,7 +15,7 @@ test('valid path', function (t) {
 test('invalid path', function (t) {
   t.plan(3)
 
-  inject(dispatch, {url: '/beep//boop'}, t.fail)
+  inject(dispatch, { url: '/beep//boop' }, t.fail)
 
   function dispatch (req, res) {
     assertPath(req, res, function (err) {
@@ -27,7 +27,7 @@ test('invalid path', function (t) {
 })
 
 test('// in query', function (t) {
-  inject(dispatch, {url: '/beep?url=http://bo.op'}, t.fail)
+  inject(dispatch, { url: '/beep?url=http://bo.op' }, t.fail)
 
   function dispatch (req, res) {
     assertPath(req, res, t.end)
